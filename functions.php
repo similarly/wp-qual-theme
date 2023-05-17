b9aa0322664e7c32b5dd16793c72ee6e8f6cbd9d
Пользователь <d.kalistov@mirenergy.local>
2023-05-17 12:29:38 +0700

    enque script

--- functions.php
+++ functions.php
@@ -42,7 +42,7 @@ function tailpress_enqueue_scripts() {
 	$theme = wp_get_theme();
 	wp_enqueue_style( 'tailpress', tailpress_asset( 'css/app.css' ), array(), $theme->get( 'Version' ) );
 	wp_enqueue_script( 'tailpress', tailpress_asset( 'js/app.js' ), array(), $theme->get( 'Version' ) );
-	wp_enqueue_script('letsee', tailpress_asset("/js/letsee.js"));
+	wp_enqueue_script('letsee', tailpress_asset("js/letsee.js"), array(), $theme->get( 'Version' ) );
 }
 
 add_action( 'wp_enqueue_scripts', 'tailpress_enqueue_scripts' );
