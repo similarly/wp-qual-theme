5c91294db613a539e1dd6bb2a6d80be88b8fc02e
Пользователь <d.kalistov@mirenergy.local>
2023-05-17 10:55:52 +0700

    changed safe file

--- safe.php
+++ safe.php
@@ -1,8 +1,8 @@
 <div class="flex flex-row justify-around">
 <div class="flex items-center justify-center">
   <a href="http://localhost/wp-content/uploads/2023/05/ustav.pdf" target="_blank" rel="noopener noreferrer">
-    <div class="relative bg-gray-200 p-4 rounded-md">
-      <img src="https://www.a-pdf.com/watermark/examples/pdf-Secret.gif" alt="Документ" class="w-40 h-40 object-cover rounded-md">
+    <div class="relative p-4 bg-gray-200 rounded-md">
+      <img src="https://www.a-pdf.com/watermark/examples/pdf-Secret.gif" alt="Документ" class="object-cover w-40 h-40 rounded-md">
       <div class="absolute inset-0 flex items-center justify-center">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-12 h-12 text-gray-500">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
@@ -14,9 +14,9 @@
 
 <div class="flex items-center justify-center">
   <a href="http://localhost/wp-content/uploads/2023/05/ustav.pdf" target="_blank" rel="noopener noreferrer" class="relative group">
-    <div class="bg-gray-200 p-4 rounded-md overflow-hidden shadow-lg transform transition duration-300 ease-in-out hover:scale-105">
-      <img src="https://www.a-pdf.com/watermark/examples/pdf-Secret.gif" alt="Документ" class="w-40 h-40 object-cover rounded-md">
-      <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+    <div class="p-4 overflow-hidden transition duration-300 ease-in-out transform bg-gray-200 rounded-md shadow-lg hover:scale-105">
+      <img src="https://www.a-pdf.com/watermark/examples/pdf-Secret.gif" alt="Документ" class="object-cover w-40 h-40 rounded-md">
+      <div class="absolute inset-0 flex items-center justify-center transition duration-300 ease-in-out opacity-0 group-hover:opacity-100">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-12 h-12 text-gray-500">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
         </svg>
@@ -26,9 +26,9 @@
 </div>
 <div class="flex items-center justify-center">
   <a href="http://localhost/wp-content/uploads/2023/05/ustav.pdf" target="_blank" rel="noopener noreferrer" class="relative group">
-    <div class="bg-white p-4 rounded-md border-2 border-blue-500 hover:border-blue-700 overflow-hidden shadow-lg transform transition duration-300 ease-in-out hover:scale-105">
-      <img src="https://www.a-pdf.com/watermark/examples/pdf-Secret.gif" alt="Документ" class="w-40 h-40 object-cover rounded-md">
-      <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+    <div class="p-4 overflow-hidden transition duration-300 ease-in-out transform bg-white border-2 border-blue-500 rounded-md shadow-lg hover:border-blue-700 hover:scale-105">
+      <img src="https://www.a-pdf.com/watermark/examples/pdf-Secret.gif" alt="Документ" class="object-cover w-40 h-40 rounded-md">
+      <div class="absolute inset-0 flex items-center justify-center transition duration-300 ease-in-out opacity-0 group-hover:opacity-100">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-12 h-12 text-gray-500">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
         </svg>
@@ -39,13 +39,13 @@
 </div>
 <div class="flex items-center justify-center">
   <div class="relative group">
-    <img src="https://www.a-pdf.com/watermark/examples/pdf-Secret.gif" alt="Документ" class="w-40 h-40 object-cover rounded-md cursor-pointer">
+    <img src="https://www.a-pdf.com/watermark/examples/pdf-Secret.gif" alt="Документ" class="object-cover w-40 h-40 rounded-md cursor-pointer">
 
     <!-- Модальное окно для отображения изображения -->
-    <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 hidden">
+    <div class="fixed inset-0 flex items-center justify-center hidden bg-black bg-opacity-50">
       <div class="relative">
         <img src="пhttps://www.a-pdf.com/watermark/examples/pdf-Secret.gif" alt="Документ" class="max-w-full max-h-full">
-        <button class="absolute top-0 right-0 mt-2 mr-2 p-2 bg-white rounded-full text-gray-500 hover:text-gray-800 transition duration-300">
+        <button class="absolute top-0 right-0 p-2 mt-2 mr-2 text-gray-500 transition duration-300 bg-white rounded-full hover:text-gray-800">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
           </svg>
@@ -69,3 +69,5 @@
     }
   });
 </script>
+
+<img class="max-w-2xl mx-auto" src="/wp-app/wp-content/uploads/2023/05/111_Плашки_этапы_работ_3.jpg" />
