b1ff40d65e78618b683f3e8df7ed24a12012a488
Пользователь <d.kalistov@mirenergy.local>
2023-05-19 12:05:36 +0700

    added category templ to fix border everywhere

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
 		<?php
 		while ( have_posts() ) :
 			?> 
-			<div class="prose max-w-none">
-			<div class="max-w-5xl p-5 mx-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 min-w-1/2"> 
 				<?php the_post(); ?> 
 				<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
 			</div>
